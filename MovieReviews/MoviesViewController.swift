699eca1f476a984a2a0a5bcfcfd71884b24dd25a
Droan Rishi <droanr@ucla.edu>
2017-09-14 21:28:08 -0700

    Instruction videos completed.

---
+++ MovieReviews/MoviesViewController.swift
@@ -0,0 +1,91 @@
+//
+//  MoviesViewController.swift
+//  MovieReviews
+//
+//  Created by drishi on 9/13/17.
+//  Copyright © 2017 Droan Rishi. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var movieTableView: UITableView!
+    
+    var movies: [NSDictionary]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        movieTableView.dataSource = self
+        movieTableView.delegate = self
+
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate: nil,
+            delegateQueue: OperationQueue.main
+        )
+        
+        let task: URLSessionDataTask = session.dataTask(with: request,
+                                                        completionHandler: {(dataOrNil, response, error) in
+                                                            if let data = dataOrNil {
+                                                                if let responseDictionary = try! JSONSerialization.jsonObject(
+                                                                    with: data, options: []) as? NSDictionary {
+                                                                    NSLog("response: \(responseDictionary)")
+                                                                    self.movies = responseDictionary["results"] as! [NSDictionary]
+                                                                    self.movieTableView.reloadData()            
+                                                                }
+                                                            }
+        })
+        task.resume()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movie = movies {
+            return (movies?.count)!
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie = movies![indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        let posterPath = movie["poster_path"] as! String
+
+        cell.movieTitle.text = title
+        cell.overviewLabel.text = overview
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w500/"
+        let posterUrl = NSURL(string: baseUrl+posterPath)
+        
+        cell.posterView.setImageWith(posterUrl! as URL)
+        print ("Row \(indexPath.row)")
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
