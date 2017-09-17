bd9d2b962d0ac32fc3533293942d1b198c15f04b
Droan Rishi <droanr@ucla.edu>
2017-09-16 18:44:23 -0700

    Add swipe to refresh

--- MovieReviews/MoviesViewController.swift
+++ MovieReviews/MoviesViewController.swift
@@ -37,7 +37,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             delegate: nil,
             delegateQueue: OperationQueue.main
         )
-        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        movieTableView.insertSubview(refreshControl, at: 0)
         MBProgressHUD.showAdded(to: self.parentView, animated: true)
         let task: URLSessionDataTask = session.dataTask(with: request,
                                                         completionHandler: {(dataOrNil, response, error) in
@@ -48,11 +50,37 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                                                                     self.movies = responseDictionary["results"] as! [NSDictionary]
                                                                     self.movieTableView.reloadData()
                                                                     MBProgressHUD.hide(for: self.parentView, animated: true)
+                                                                    
                                                                 }
+                                                                
                                                             }
         })
         task.resume()
         
+    }
+    
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate: nil,
+            delegateQueue: OperationQueue.main
+        )
+        let task: URLSessionDataTask = session.dataTask(with: request,
+                                                        completionHandler: {(dataOrNil, response, error) in
+                                                            if let data = dataOrNil {
+                                                                if let responseDictionary = try! JSONSerialization.jsonObject(
+                                                                    with: data, options: []) as? NSDictionary {
+                                                                    NSLog("response: \(responseDictionary)")
+                                                                    self.movies = responseDictionary["results"] as! [NSDictionary]
+                                                                    self.movieTableView.reloadData()
+                                                                    refreshControl.endRefreshing()
+                                                                }
+                                                            }
+        })
+        task.resume()
     }
     
     func isInternetAvailable() -> Bool
